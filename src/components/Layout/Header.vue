a9847ec3193b779b5318d3362e1acf8780d075fa
Fatima Abarder <fatima@io.co.za>
2020-07-02 08:41:50 +0200

    First commit

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <RouterLink to="/">
+        Home
+      </RouterLink><RouterLink to="/about">
+        About
+      </RouterLink>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style scoped>
+    .Header {
+        background: #333;
+        color: #fff;
+        text-align:center;
+        padding: 10px;
+}
+
+    .Header a {
+        color: #fff;
+        padding-right: 5px;
+        text-decoration: none;
+}
+
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div
+      v-for="(todo, index) in todos"
+      :key="index"
+    >
+      <h3>{{ todo.title }}</h3>
+            
+      <TodoItem
+        :todo="todo"
+        @del-todo="$emit('del-todo',
+                         todo.id)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "Todos",
+    components:{
+      
+},
+    // eslint-disable-next-line vue/require-prop-types
+    props: ["todos"]
+
+}
+</script>
+
+<style scoped>
+
+</style>
